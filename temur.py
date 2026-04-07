77c7232fdca9c72a339c77291830aa83e8fa0d84
Tim <tomtemurov30@email.com>
2026-04-07 14:29:50 +0500

    Saving my assignment files before pulling

---
+++ temur.py
@@ -0,0 +1,8 @@
+def calculate_area(*radius):
+    for pi in radius:
+        if pi >= 10:
+            print(pi % 2 == 0)
+        else:
+            print(pi % 3 == 0)
+
+calculate_area(23, 2, 3, 4, 12, 14)
